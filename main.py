ae6dbe6fbb588989e3dcb057415d04742f32cc6d
other019 <rzepkafoto@gmail.com>
2015-06-30 23:00:00 +0200

    init

---
+++ main.py
@@ -0,0 +1,14 @@
+__author__ = 'other019'
+
+
+class Pole:
+    mozliwosci = [True for i in xrange(10)]
+    wartosc = 0
+
+
+class Plansza:
+    tablica = [[0 for i in xrange(9)] for i in xrange(9)]
+
+    def wczytaj(self):
+
+__author__ = 'other019'
