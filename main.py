6f4d91fded290fb9e5c5ec6b5b5caa9a3292a005
other019 <rzepkafoto@gmail.com>
2015-06-30 23:49:45 +0200

    base classes

--- main.py
+++ main.py
@@ -1,14 +1,49 @@
 __author__ = 'other019'
 
 
-class Pole:
-    mozliwosci = [True for i in xrange(10)]
-    wartosc = 0
+class Square:
+    possibilities = [True for i in xrange(10)]
+    value = 0
+
+    def __init__(self):
+        pass
+
+    def set_value(self, val):
+        self.value = val
+
+    def get_value(self):
+        return self.value
 
 
-class Plansza:
-    tablica = [[0 for i in xrange(9)] for i in xrange(9)]
+class Board:
+    tab = [[Square() for i in xrange(9)] for j in xrange(9)]
 
-    def wczytaj(self):
+    def __init__(self):
+        self.read()
 
-__author__ = 'other019'
+    def read(self):
+        for i in xrange(9):
+            row = raw_input()
+            row = row.split()
+            for j in xrange(9):
+                self.tab[i][j].set_value(row[j])
+
+    def __str__(self):
+        res = '[\n'
+        for i in xrange(9):
+            res += '['
+            for j in xrange(9):
+                res += self.tab[i][j].get_value()
+                res += ', '
+            res += ']\n'
+        res += ']'
+        return res
+
+
+def main():
+    board = Board()
+    print board
+
+
+if __name__ == '__main__':
+    main()
